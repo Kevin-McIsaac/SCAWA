bf30faf02680afde9a8325a5d258b0bfde0c5053
Fractal Feelings <kevin.mcisaac@gmail.com>
2023-06-02 09:21:41 +0200

    init

--- simon.py
+++ simon.py
@@ -17,6 +17,32 @@ import os
 from dotenv import load_dotenv
 load_dotenv() #get API keys
 
+PROMPT = '''You are a helpful Strata legal expert in Western Australia answering questions about the "Strata Titles Act 1985" from a lot owner.
+
+Start the answer with "An owner should always refer to their bylaws and strata plan in conjenction with the legislation".
+
+Provide a detailed answer using the information from the legislation provided below. List relevant sections of the act. 
+
+Do not make up answers. If you do not know say "I do not know".
+
+    {context}
+
+Question: {question}'''
+
+class SimpleStreamlitCallbackHandler(BaseCallbackHandler):
+    """ Copied only streaming part from StreamlitCallbackHandler """
+    
+    def __init__(self) -> None:
+        self.tokens_area = st.empty()
+        self.tokens_stream = ""
+        
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        """Run on new LLM token. Only available when streaming is enabled."""
+        self.tokens_stream += token
+        self.tokens_area.markdown(self.tokens_stream)
+
+st.markdown('<style>[data-testid="stForm"] {border: 0px}</style>', unsafe_allow_html=True) # no form border 
+
 @st.cache_resource
 def makeBot(prompt_template, k, streaming, namespace):
     '''Set up connection to db and create the Qretrieval object
@@ -43,51 +69,31 @@ def makeBot(prompt_template, k, streaming, namespace):
 
 st.title("Simon's Playground")
 st.caption("Use the inputs in the sidebar to experiment with the playground settings")
-           
-with st.sidebar:
-    
-    namespace = 'SCA_H5'
-    k = st.number_input("How many Articles should Simon consult", value= 4, 
-                        help="This determines the number of similar article chunks to use in the prompt. Increasing this adds to the knowledge available but increases the cost/time to answer the inquiry")
 
-    streaming = st.radio("Stream answers?", ("Yes", "No"),horizontal=True, index=0,
-                         help="Streaming shows the output as its generated like chatGPT but the cost and #tokens does not work. Turn off to see costs")
-    
-    
-    instructions = st.text_area('Instructions for Simon to answer the inquiry', 
-                                value=
-'''You are a helpful Strata legal expert in Western Australia answering questions about the "Strata Titles Act 1985" from a lot owner.
-
-Start the answer with "An owner should always refer to their bylaws and strata plan in conjenction with the legislation".
-
-Provide a detailed answer using the information from the legislation provided below. List relevant sections of the act. 
-
-Do not make up answers. If you do not know say "I do not know".
-
-{context}
-
-Question: {question}
-''',
-                                height = 300, 
-                                help='This is prepended to the inquiry and provides instructions on how to answer the inqueiry. Try chaning this to get a better result.')
-
-
-qa=makeBot(instructions, k, streaming, namespace) 
-inquiry = st.text_area(f"Hi, I'm Simon, the Strata Chatbot? What is your inquiry?",
-                       value="")
-class SimpleStreamlitCallbackHandler(BaseCallbackHandler):
-    """ Copied only streaming part from StreamlitCallbackHandler """
-    
-    def __init__(self) -> None:
-        self.tokens_area = st.empty()
-        self.tokens_stream = ""
+with st.form("chat"):           
+    with st.sidebar:
         
-    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-        """Run on new LLM token. Only available when streaming is enabled."""
-        self.tokens_stream += token
-        self.tokens_area.markdown(self.tokens_stream)
+        namespace = 'SCA_H5'
+        k = st.number_input("How many Articles should Simon consult", value= 4, 
+                            help="This determines the number of similar article chunks to use in the prompt. Increasing this adds to the knowledge available but increases the cost/time to answer the inquiry")
 
-if inquiry:
+        streaming = st.radio("Stream answers?", ("Yes", "No"),horizontal=True, index=0,
+                            help="Streaming shows the output as its generated like chatGPT but the cost and #tokens does not work. Turn off to see costs")
+        
+        
+        instructions = st.text_area('Instructions for Simon to answer the inquiry', 
+                                    value=PROMPT,
+                                    height = 300, 
+                                    help='This is prepended to the inquiry and provides instructions on how to answer the inqueiry. Try chaning this to get a better result.')
+
+
+    qa=makeBot(instructions, k, streaming, namespace) 
+    inquiry = st.text_area(f"Hi, I'm Simon, the Strata Chatbot? What is your inquiry?",
+                        value="")
+    submitted = st.form_submit_button("Submit")
+
+
+if submitted:
     with get_openai_callback() as costs:
         result = qa(inquiry, callbacks=[SimpleStreamlitCallbackHandler()]) 
         #streaming callback handler breaks cost reporting
