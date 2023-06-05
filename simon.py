62b2d4bca4c9acce929785d9dc22ec080d51ee7b
Fractal Feelings <kevin.mcisaac@gmail.com>
2023-06-05 09:30:05 +0200

    footer

--- simon.py
+++ simon.py
@@ -108,12 +108,12 @@ if submitted:
 
 
 
-FOOTER='''<font size="1">
+FOOTER='''<sub>
 This webpage provides a basic understanding of strata title living. It is general information only 
-and is not legal advice on strata titles. You should refer to the legislation available on the WA government website. \n\n                                                          
+and is not legal advice on strata titles. You should refer to the legislation available on the WA government website.                                                          
 To the extent permitted by law, we will in no way be liable to you or anyone else for any loss or damage,
 however caused (including through negligence), which may be directly or indirectly suffered in connection with use
 of this document.
-</font>'''
+</sub>'''  
 
-st.markdown(FOOTER, unsafe_allow_html=True)
+st.markdown(FOOTER, unsafe_allow_html=True )
